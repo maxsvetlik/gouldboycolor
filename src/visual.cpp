8708079cbf628f7a9a5fc4684d71fdaa00cdf7ee
Maxwell Svetlik <maxsvetlik@utexas.edu>
2016-04-09 12:15:59 -0500

    added support for the color pallete register and LCDC register, allowing greater adherence to the internal state of the gameboy

--- src/visual.cpp
+++ src/visual.cpp
@@ -1,5 +1,5 @@
 /* This file will handle (as a minimum) the visualization of the emulator 
- * in the form of interfacing with a user's screen.
+ * in the form of interfacing with a user's input.
  *
  * Author Maxwell J Svetlik
  */
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 #include "visualization.h"
-#include "video.h"
+#include "video_const.h"
 
 const int SCREEN_WIDTH = 166;
 const int SCREEN_HEIGHT = 140;
@@ -16,19 +16,23 @@ const int SCREEN_HEIGHT = 140;
 const int BUFFER_WIDTH = 255;
 const int BUFFER_HEIGHT = 255;
 
-Uint32 trans;   //transparent 
+Uint32 white;   //transparent 
 Uint32 lgray;   //light gray
 Uint32 gray;    //gray
 Uint32 dgray;   //dark gray
+Uint32 black;   //black
+
+unsigned char color_map[4];
 
 SDL_Surface* surface;
 SDL_Window* window;
 
 void initilize_colors(SDL_Surface *surface){
-    trans = SDL_MapRGBA(surface->format, 255,255,255,0xFF); 
+    white = SDL_MapRGBA(surface->format, 255,255,255,0xFF); 
     lgray = SDL_MapRGBA(surface->format, 195, 195, 195, 0xFF);
     gray = SDL_MapRGBA(surface->format, 100, 100, 100, 0xFF);
     dgray = SDL_MapRGBA(surface->format, 15, 15, 15, 0xFF);
+    black = SDL_MapRGBA(surface->format, 0, 0, 0, 0xFF);
 }
 Uint32 get_pixel32( SDL_Surface *surface, int x, int y ) { 
     Uint32 *pixels = (Uint32 *)surface->pixels; 
@@ -85,17 +89,18 @@ void draw_line(unsigned int line[], int x, int y){
     int pallet_col;
     for(i = 0; i < 8; i+=1){
         pallet_col = line[i];
-        if(pallet_col  == 0)
+        color = color_map[pallet_col];
+        if(color  == WHITE)
+            color = white;
+        else if(color  == LGRAY)
             color = lgray;
-        else if(pallet_col  == 2)
+        else if(color == DGRAY)
             color = dgray;
-        else if(pallet_col == 3)
-            color = dgray;
-        else if(pallet_col == 1)
-            color = trans;
+        else if(color == BLACK)
+            color = black;
         pixels[(y * surface->w) + x + i] = color;
     }
-    SDL_UpdateWindowSurface( window );    
+       
 }
 
 /* Draw all the tiles referenced in 0x8800 to 8fff or 0x8800 to 97ff depending on LCDC reg.
@@ -106,42 +111,79 @@ void draw_line(unsigned int line[], int x, int y){
  * In this area, colors for pixels within a tile is determined by a two byte quantity, the first describing the LSBs and the 2nd describing the MSBs
  */
 void draw_tile(unsigned char mem[]){
-    int start = 0x9800;
-    int tile_base = 0x8000;
     unsigned int draw_vect[8];
     int i,j,k;
     int x = 0; int y = 0;
-    //mem[0xFF44] = 0x90;
-    int tempy = 0;
-    for(i = 0; i < 1024; i+=1){
-        unsigned char tile_num = mem[start+i]; 
+
+    int start = 0x9800;
+    int tile_base = 0x8000;
+    char signed_tiles = 0;
+    //check the LCDC register, set approriate flags
+    unsigned char lcdc = mem[LCDC];
+    char lcd_enable = !!(lcdc & 0x80);
+    if(lcd_enable){
+        //TODO:set window tile map display region
+        //TODO:set window display enable
+        //TODO:set Background and Window tile region
+        if(!(lcdc & 0x10)){
+            tile_base = 0x8800;
+            signed_tiles = 1;
+        }
+        else {
+            tile_base = 0x8000;
+            signed_tiles = 0;
+        }
+
+        //set tile map display region
+        if(!(lcdc & 0x8))
+            start = 0x9800;
+        else
+            start = 0x9C00;
         
-        //draw one single tile reference
-        for(k = 0; k < 16; k+=2){
-            unsigned char lsb = mem[tile_base+(16*tile_num)+k];     //every tile is 16 bytes
-            unsigned char msb = mem[tile_base+(16*tile_num)+k+1];   // ...
-            //if(lsb || msb)
-            //    printf("Found lsb: %x msb: %x at %x from map %x\n", lsb, msb, tile_base+tile_num, start+i);
+        //TODO:set sprite size
+        //TODO:set sprite display enable
+        //TODO:set background display
+
+        //set the internal color pallete
+        unsigned char pallete = mem[BGP];
+        color_map[0] = pallete & 0x3;
+        color_map[1] = (pallete >> 2) & 0x3;
+        color_map[2] = (pallete >> 4) & 0x3;
+        color_map[3] = (pallete >> 6) & 0x3;
+
+        for(i = 0; i < 1024; i+=1){
+            unsigned char tile_num = mem[start+i]; 
             
-            draw_vect[0] = ((msb & 1 << 7) >> 6) + ((lsb & 1 << 7) >> 7);
-            draw_vect[1] = ((msb & 1 << 6) >> 5) + ((lsb & 1 << 6) >> 6);
-            draw_vect[2] = ((msb & 1 << 5) >> 4) + ((lsb & 1 << 5) >> 5);
-            draw_vect[3] = ((msb & 1 << 4) >> 3) + ((lsb & 1 << 4) >> 4);
-            draw_vect[4] = ((msb & 1 << 3) >> 2) + ((lsb & 1 << 3) >> 3);
-            draw_vect[5] = ((msb & 1 << 2) >> 1) + ((lsb & 1 << 2) >> 2);
-            draw_vect[6] = ((msb & 1 << 1) >> 0) + ((lsb & 1 << 1) >> 1);
-            draw_vect[7] = ((msb & 1 << 0) << 1) + ((lsb & 1 << 0) >> 0);
-            
-            draw_line(draw_vect, x, y+(k/2));
-            
+            //draw one single tile reference
+            for(k = 0; k < 16; k+=2){
+                unsigned char lsb = mem[tile_base+(16*tile_num)+k];     //every tile is 16 bytes
+                unsigned char msb = mem[tile_base+(16*tile_num)+k+1];   // ...
+                //if(lsb || msb)
+                //    printf("Found lsb: %x msb: %x at %x from map %x\n", lsb, msb, tile_base+tile_num, start+i);
+                
+                draw_vect[0] = ((msb & 1 << 7) >> 6) + ((lsb & 1 << 7) >> 7);
+                draw_vect[1] = ((msb & 1 << 6) >> 5) + ((lsb & 1 << 6) >> 6);
+                draw_vect[2] = ((msb & 1 << 5) >> 4) + ((lsb & 1 << 5) >> 5);
+                draw_vect[3] = ((msb & 1 << 4) >> 3) + ((lsb & 1 << 4) >> 4);
+                draw_vect[4] = ((msb & 1 << 3) >> 2) + ((lsb & 1 << 3) >> 3);
+                draw_vect[5] = ((msb & 1 << 2) >> 1) + ((lsb & 1 << 2) >> 2);
+                draw_vect[6] = ((msb & 1 << 1) >> 0) + ((lsb & 1 << 1) >> 1);
+                draw_vect[7] = ((msb & 1 << 0) << 1) + ((lsb & 1 << 0) >> 0);
+                
+                draw_line(draw_vect, x, y+(k/2));
+                
+            }
+            if(x >= 248){ 
+                x = 0; 
+                y += 8;
+            }
+            else 
+                x += 8;
         }
-        if(x >= 248){ 
-            x = 0; 
-            y += 8;
-        }
-        else 
-            x += 8;
     }
+
+    //update window only after all lines are updated
+    SDL_UpdateWindowSurface( window ); 
 }
 
 //Exit SDL cleanly
